aeaf91c380b07807ff2b5bc4d7f1fc8cceb82b27
Rajat Bhageria <rajat1996@gmail.com>
2017-04-14 18:09:02 -0400

    Added background and images
    Added some background images, added Demo sunrise button, and added
    colors to Settings Page

---
+++ Alarm/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  Alarm-ios-swift
+//
+//  Created by Rajat Bhageria on 4/13/17.
+//  Copyright © 2017 LongGames. All rights reserved.
+//
+
+import Foundation
