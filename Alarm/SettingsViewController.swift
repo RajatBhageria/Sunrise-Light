f18b2bbbde25601a9f63f574c87dab3a7c6496c5
Rajat Bhageria <rajat1996@gmail.com>
2017-04-14 18:12:48 -0400

    White text

--- Alarm/SettingsViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SettingsViewController.swift
-//  Alarm-ios-swift
-//
-//  Created by Rajat Bhageria on 4/13/17.
-//  Copyright © 2017 LongGames. All rights reserved.
-//
-
-import Foundation
