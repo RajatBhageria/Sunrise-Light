e1dc6af2e8488595baaf2a093e5d5fd6cacc55e6
Rajat Bhageria <rajat1996@gmail.com>
2017-04-15 11:49:40 -0400

    Fixed sigbart error

---
+++ Alarm/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Alarm-ios-swift
+//
+//  Created by Rajat Bhageria on 4/15/17.
+//  Copyright © 2017 LongGames. All rights reserved.
+//
+
+import Foundation
+
+class SettingsViewController: UIViewController {
+    
+}
